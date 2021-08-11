3b4f0be628314efc2eab6bd7bcd4cc219d8c1724
Morgan French <mfrench1989@gmail.com>
2021-08-11 13:45:05 -0700

    Basic definitions

---
+++ define_default.hpp
@@ -0,0 +1,16 @@
+#ifndef DEFINE_DEFAULT_HPP
+#define DEFINE_DEFAULT_HPP
+
+#include <QDebug>
+
+#define FILE_LOG ".log"
+
+enum class Event_Type {
+  Debug,
+  Error,
+  Warning,
+  Success,
+  Default
+};
+
+#endif // DEFINE_DEFAULT_HPP
