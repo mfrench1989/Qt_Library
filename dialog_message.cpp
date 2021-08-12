6e6434bd8a7cd975116663cfcdab5501f06fdf39
Morgan French <mfrench1989@gmail.com>
2021-08-12 16:33:59 -0700

    Added dialog message for displaying message popups

---
+++ dialog_message.cpp
@@ -0,0 +1,38 @@
+#include <QSettings>
+
+#include "function_interface.hpp"
+
+#include "dialog_message.hpp"
+#include "ui_dialog_message.h"
+
+DialogMessage::DialogMessage(std::string text_in, QWidget *parent) : QDialog(parent), ui(new Ui::DialogMessage) {
+  ui->setupUi(this);
+  this->setModal(false);
+  if (parent) {
+      this->setObjectName(this->parent()->objectName() + "Message");
+    }
+  else {
+      this->setObjectName("Message");
+    }
+
+  /*Load position on creation*/
+  QSettings overseer_settings(qApp->objectName(), "Dialog");
+  overseer_settings.beginGroup(this->objectName());
+  restoreGeometry(overseer_settings.value("Position").toByteArray());
+  overseer_settings.endGroup();
+
+  /*Format UI elements*/
+  guiFormatWidget(*this, HUE_BASE, SAT_BASE, 15);
+  guiFormatLabel(*ui->label_Message, HUE_BASE, SAT_GRAY, LUM_GRAY, false);
+
+  ui->label_Message->setText(QString::fromStdString(text_in));
+}
+
+DialogMessage::~DialogMessage() {
+  /*Save position on close*/
+  QSettings overseer_settings(qApp->objectName(), "Dialog");
+  overseer_settings.beginGroup(this->objectName());
+  overseer_settings.setValue("Position", saveGeometry());
+  overseer_settings.endGroup();
+  delete ui;
+}
---
+++ dialog_message.hpp
@@ -0,0 +1,21 @@
+#ifndef DIALOG_MESSAGE_HPP
+#define DIALOG_MESSAGE_HPP
+
+#include <QDialog>
+
+namespace Ui {
+  class DialogMessage;
+}
+
+class DialogMessage : public QDialog {
+  Q_OBJECT
+
+public:
+  explicit DialogMessage(std::string = "", QWidget* = nullptr);
+  ~DialogMessage();
+
+private:
+  Ui::DialogMessage *ui;
+};
+
+#endif // DIALOG_MESSAGE_HPP
